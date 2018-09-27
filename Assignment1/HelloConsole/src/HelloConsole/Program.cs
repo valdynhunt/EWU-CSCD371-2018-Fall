b81abf1e716330c6665a2f86af3e6ce3c1cb17a5
Valdyn Hunt <valdyn@mac.com>
2018-09-26 23:18:08 -0700

    add HelloConsole and HelloConsole.Tests to solution

---
+++ Assignment1/HelloConsole/src/HelloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloConsole
+{
+    public class Greeting
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from your friendly neighborhood console!");
+        }
+    }
+}
---
+++ Assignment1/HelloConsole/test/HelloConsole.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloConsole.Tests
+{
+    [TestClass]
+    public class ConsoleGreetingTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
