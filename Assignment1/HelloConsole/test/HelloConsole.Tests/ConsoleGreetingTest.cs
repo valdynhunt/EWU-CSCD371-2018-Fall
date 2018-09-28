720d1828dbfead0608ba4ecc05e99a64bb0d268f
Valdyn Hunt <valdyn@mac.com>
2018-09-27 19:55:51 -0700

    modify string interpolation syntax

--- Assignment1/HelloConsole/test/HelloConsole.Tests/ConsoleGreetingTest.cs
+++ Assignment1/HelloConsole/test/HelloConsole.Tests/ConsoleGreetingTest.cs
@@ -8,12 +8,15 @@ namespace HelloConsole.Tests
         [TestMethod]
         public void TestInitialGreeting()
         {
-            string expectedOutput = @">>Hello from your friendly neighborhood console!
+            // test with male response - add branch w female later
+            string firstName = "Alfred";
+            string lastName = "Newman";
+            string expectedOutput = $@">>Hello from your friendly neighborhood console!
 >>What is your first name?
-<<Alfred
->>Thanks Alfred! What is your last name?
-<<Newman
->>Excellent! Greetings to you, Sir Alfred Newman!
+<<{firstName}
+>>Thanks {firstName}! What is your last name?
+<<{lastName}
+>>Excellent! Greetings to you, Sir {firstName} {lastName}!
 >>I hope this day finds you well and productive.";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOutput, HelloConsole.Greeting.Main);
