88a054965b5f954f5995d90afdffee6b5423d6a6
Valdyn Hunt <valdyn@mac.com>
2018-10-04 18:24:00 -0700

    modify tests

--- Assignment2/MathExpressions/test/MathExpressions.Tests/MathExpressionsTests.cs
+++ Assignment2/MathExpressions/test/MathExpressions.Tests/MathExpressionsTests.cs
@@ -7,20 +7,141 @@ namespace MathExpressions.Tests
     public class MathExpressionsTests
     {
         [TestMethod]
-        public void TestInitialGreeting()
+        public void TestPrintIntro()
         {
-            // test with male response - add branch w female later
-            string firstName = "Alfred";
-            string lastName = "Newman";
-            string expectedOutput = $@">>Hello from your friendly neighborhood console!
->>What is your first name?
-<<{firstName}
->>Thanks {firstName}! What is your last name?
-<<{lastName}
->>Excellent! Greetings to you, Sir {firstName} {lastName}!
->>I hope this day finds you well and productive.";
 
-            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOutput, MathExpressions.Main);
+            string expectedOutput = $@">>Hello from the Math Expression Console!
+>>Please enter a math expression of the form <int><operator><int>.
+>>The operator can be +, -, *, or /, and the int can be negative.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expectedOutput, MathExpressions.PrintIntro);
+        }
+
+
+        [TestMethod]
+        public void TestFindOperator()
+        {
+            string expr = "3+4";
+            char expectedOutput = '+';
+            Assert.AreEqual(expectedOutput, MathExpressions.FindOperator(expr));
+
+            expr = "7*9";
+            expectedOutput = '*';
+            Assert.AreEqual(expectedOutput, MathExpressions.FindOperator(expr));
+
+            expr = "8/4";
+            expectedOutput = '/';
+            Assert.AreEqual(expectedOutput, MathExpressions.FindOperator(expr));
+
+            expr = "6-5";
+            expectedOutput = '-';
+            Assert.AreEqual(expectedOutput, MathExpressions.FindOperator(expr));
+        }
+
+
+//        [TestMethod]
+//        public void TestPrintIntro()
+//        {
+
+//            string expectedOutput = $@">>Hello from the Math Expression Console!
+//>>Please enter a math expression of the form <int><operator><int>.
+//>>The operator can be +, -, *, or /, and the int can be negative.";
+
+//            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+//                expectedOutput, MathExpressions.PrintIntro);
+//        }
+
+
+//        [TestMethod]
+//        public void TestPrintIntro()
+//        {
+
+//            string expectedOutput = $@">>Hello from the Math Expression Console!
+//>>Please enter a math expression of the form <int><operator><int>.
+//>>The operator can be +, -, *, or /, and the int can be negative.";
+
+//            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+//                expectedOutput, MathExpressions.PrintIntro);
+//        }
+
+
+//        [TestMethod]
+//        public void TestPrintIntro()
+//        {
+
+//            string expectedOutput = $@">>Hello from the Math Expression Console!
+//>>Please enter a math expression of the form <int><operator><int>.
+//>>The operator can be +, -, *, or /, and the int can be negative.";
+
+//            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+//                expectedOutput, MathExpressions.PrintIntro);
+//        }
+
+
+//        [TestMethod]
+//        public void TestPrintIntro()
+//        {
+
+//            string expectedOutput = $@">>Hello from the Math Expression Console!
+//>>Please enter a math expression of the form <int><operator><int>.
+//>>The operator can be +, -, *, or /, and the int can be negative.";
+
+//            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+//                expectedOutput, MathExpressions.PrintIntro);
+//        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+/*
+
+    Support the four basis arithmetic operators:
+addition (+)
+subtractions (-)
+multiplication (*)
+division (/)
+    
+Integers may be positive or negative.
+
+Test potential failure cases:
+zero
+int.MinValue
+or int.MaxValue
+
+Remember that a unit test should test only a single condition, not multiple conditions. 
+For example rather than writing a unit test for addition, create separate unit tests to handle
+
+=============================================================================
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_03.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Main_WriteInventions()
+        {
+            const string expected =
+@"Bifocals (1784)
+Phonograph (1877)";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
         }
     }
 }
+===============================================================================
+*/
