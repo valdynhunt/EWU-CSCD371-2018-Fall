4e3dba266cdde316a7196cd1e358ae0526c22f75
Valdyn Hunt <valdyn@mac.com>
2018-10-09 07:33:08 -0700

    initial solution setup

---
+++ Assignment3/Roshambo/src/Roshambo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Roshambo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Assignment3/Roshambo/test/Roshambo.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Roshambo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
