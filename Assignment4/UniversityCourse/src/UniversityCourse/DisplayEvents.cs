95ee531e9130e97078be48795ae2dfde7d6ba85b
Valdyn Hunt <valdyn@mac.com>
2018-10-20 12:53:49 -0700

    add DisplayEventsTests class

---
+++ Assignment4/UniversityCourse/src/UniversityCourse/DisplayEvents.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniversityCourse
+{
+
+    public static class DisplayEvents
+    {
+        public static void DisplayEventCollection(List<Event> EventList)
+        {
+
+            foreach (Object @object in EventList)
+            {
+                Display(@object);
+            }
+
+        }
+
+        public static void Display(Object @object)
+        {
+            switch (@object)
+            {
+                case Event ev:
+                    if (ev.GetType().GetMethod("GetSummaryInformation") != null)
+                    {
+                        Console.WriteLine(ev.GetSummaryInformation());
+                    }
+                    else
+                    {
+                        Console.WriteLine(ev.ToString());
+                    }
+                    break;
+                case null:
+                    throw new ArgumentNullException(nameof(@object));
+                default:
+                    throw new ArgumentException("Unknown object type", nameof(@object));
+            }
+        }
+    }
+}
+
---
+++ Assignment4/UniversityCourse/test/UniversityCourse.Tests/DisplayEventsTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityCourse.Tests
+{
+    class DisplayEventsTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
