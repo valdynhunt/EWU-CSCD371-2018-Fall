88eb2f0230706656f5f5570c7571d59b22b15643
Valdyn Hunt <valdyn@mac.com>
2018-10-29 17:00:27 -0700

    initial commit

---
+++ Assignment5/EventApp/src/EventApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Assignment5/EventApp/test/EventApp.Tests/EventAppTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventApp.Tests
+{
+    [TestClass]
+    public class EventAppTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
