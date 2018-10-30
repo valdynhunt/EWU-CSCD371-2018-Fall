51ab03020d12e7d563c2333f8a05fd5de4a00e3d
Valdyn Hunt <valdyn@mac.com>
2018-10-30 16:38:55 -0700

    test null with is and as

--- Assignment5/EventApp/test/EventApp.Tests/EventAppTests.cs
+++ Assignment5/EventApp/test/EventApp.Tests/EventAppTests.cs
@@ -70,5 +70,33 @@ namespace EventApp.Tests
             Assert.AreEqual(ev as UniversityCourse, course as UniversityCourse);
 
         }
+
+        [TestMethod]
+        public void Failed_Conversion_With_as_Fails_Gracefully()
+
+        {
+            UniversityCourse course = new UniversityCourse("Programming 1", "MWF 9a - 10a", "An intro to programming using Python.",
+"Cheney, WA", 45, 13254, "CSCD", 211, 1, "Steiner", "Computing and Engineering Bldg.", 107, 5, 0);
+
+            Object ob = course;
+            Object ob2 = "foo";
+
+            Assert.AreEqual(ob, ob as UniversityCourse);
+            Assert.AreEqual(ob2 as UniversityCourse, null);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Failed_Conversion_With_is_Throws()
+        {
+            UniversityCourse course = new UniversityCourse(null, "MWF 9a - 10a", "An intro to programming using Python.",
+                "Cheney, WA", 45, 13254, null, 811, 1, "Steiner", "Computing and Engineering Bldg.", 107, 5, 0);
+
+            Object ob = "foo";
+            Assert.AreEqual(ob is UniversityCourse, null);
+            Assert.IsTrue(ob is UniversityCourse myCourse && myCourse.Credits == 5);
+
+        }
     }
 }
