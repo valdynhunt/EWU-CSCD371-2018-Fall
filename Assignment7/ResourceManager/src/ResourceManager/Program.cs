dfbf005e624eabd182990bd10ab9fc553cf649eb
Valdyn Hunt <valdyn@mac.com>
2018-11-05 22:31:02 -0800

    initial commit

---
+++ Assignment7/ResourceManager/src/ResourceManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResourceManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
