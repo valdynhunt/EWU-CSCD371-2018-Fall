10f166f39a919a1d9c2557a4e531adf542f7cb50
Kevin B <kitokeboo@gmail.com>
2018-11-13 17:21:36 -0800

    Updates with more WPF goodness

---
+++ Lectures/2018.11.8-WPF/LearnWpf/LearnWpf/NewWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LearnWpf
+{
+    /// <summary>
+    /// Interaction logic for NewWindow.xaml
+    /// </summary>
+    public partial class NewWindow : Window
+    {
+        public NewWindow()
+        {
+            DataContext = new NewWindowViewModel();
+            InitializeComponent();
+        }
+
+        private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Debug.WriteLine($"Text is: {((TextBox)sender).Text}");
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            ((NewWindowViewModel) DataContext).Name = "Mark";
+        }
+    }
+}
