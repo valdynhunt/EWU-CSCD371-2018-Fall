13bc0edd2d6ecf023237afd6583fd21452cb35e4
Valdyn Hunt <valdyn@mac.com>
2018-12-03 15:07:54 -0800

    initial commit

---
+++ Assignment9/PatentDataAnalyzer/src/PatentDataAnalyzer/PatentDataAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatentDataAnalyzer
+{
+    public class PatentDataAnalyzer
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Assignment9/PatentDataAnalyzer/test/PatentDataAnalyzer.Tests/PatentDataAnalyzerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PatentDataAnalyzer.Tests
+{
+    [TestClass]
+    public class PatentDataAnalyzerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
